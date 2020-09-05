b0cd43f58dea13302fbc9751946906a56d1af28f
Dianne <dianne.diazm@gmail.com>
2020-09-05 13:01:11 -0400

    segundo commit

---
+++ assets/js/javascript.js
@@ -0,0 +1,107 @@
+$(document).ready(function() {
+    let formulario = $("#formulario");
+    let pokenumero = parseInt($("#pokenumero").val()) || Math.floor(Math.random() * 100);
+    let expresion = /\d/gmi;
+    console.log(pokenumero);
+    
+
+formulario.on("submit", function (event) {
+        event.preventDefault();
+        pokenumero = parseInt($("#pokenumero").val());
+        console.log(pokenumero);
+        consulta(pokenumero);
+});
+
+function consulta(pokenumero) {
+    if (pokenumero && expresion.test(pokenumero) && pokenumero > 0 && pokenumero <= 893) {
+        $.ajax ({
+            datatype: "json",
+            type: "get",
+            url: "https://pokeapi.co/api/v2/pokemon/1" + pokenumero,
+            success: function (response) {
+                console.log(response);
+                $("#resultado").html(`
+                <div class="text-center">
+                    <h3>Nombre: ${response.name}</h3>
+                    <img src="${response.sprites.front_default}" alt="${response.name}">
+                </div>
+            `);
+            let resultado = `
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>Slot</th>
+                        <th>Habilidad</th>
+                        <th>Oculto</th>
+                    </tr>
+                </thead>
+                <tbody>
+            `;
+            response.abilities.forEach(element => {
+                console.log(element.ability.name);
+                resultado += `
+                    <tr>
+                        <th>${element.slot}</th>
+                        <td>${element.ability.name}</td>
+                        <td>${element.is_hidden}</td>
+                    </tr>
+                `;
+            });
+            resultado += `
+                </tbody>
+                </table>
+            `;
+            $('#resultado').append(resultado);
+            
+
+            let datosXY = [];
+            response.stats.forEach(element => {
+                console.log(element.base_stat);//y
+                console.log(element.stat.name);//label
+                datosXY.push(
+                    {
+                        label: element.stat.name, 
+                        y:element.base_stat
+                    });
+            });
+
+            var options = {
+                title: {
+                    text: "Grafica de columnas con jQuery y CanvasJS"              
+                },
+                data: [              
+                    {
+                        type: "column",
+                        dataPoints: datosXY
+                    }
+                ]
+            };
+        
+            $("#chartContainer").CanvasJSChart(options);
+        },
+        error: function (error) {
+            console.error(error);
+        }
+    });
+
+        } else {
+            alert("Ingrese un número entre el 1 y el 893")
+        }; 
+        }
+        });
+
+
+
+
+                    
+                
+
+
+
+
+
+
+
+
+
+
